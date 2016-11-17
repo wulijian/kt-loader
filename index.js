04a846080266679d91ed4e7ab6820b0aedc4d0f9
KnightWu <wulijian722@gmail.com>
2016-11-17 17:10:42 +0800

    MOD: 模板路径可配置

--- index.js
+++ index.js
@@ -3,14 +3,18 @@
  */
 var path = require("path");
 var beautify = require('js-beautify').js_beautify;
+var beautify = require('js-beautify').js_beautify;
 
 module.exports = function (source, sourcemap) {
-    this.cacheable();
+    var templateDir = path.normalize(this.query.replace(/^\?/, ''));
     var callback = this.async();
+    var modeNameReg = new RegExp(templateDir.replace(/([\\\/])/g, function ($0, $1) {
+            return $1 + $1;
+        }) + '([\\\/\\\\])(.*)\\1.*$');
     if (sourcemap.sources.length > 0) {
-        var modenamem = sourcemap.sources[0].match(/template\/(.*)\/.*$/);
+        var modenamem = sourcemap.sources[0].match(modeNameReg);
         if (modenamem !== null) {
-            callback(null, beautify(require('./lib/modules').buildTemplate(path.resolve(process.cwd(), './src/template'), modenamem[1]), {indent: 4}));
+            callback(null, beautify(require('./lib/modules').buildTemplate(path.resolve(process.cwd(), templateDir), modenamem[2]), {indent: 4}));
         } else {
             callback(null, source);
         }
